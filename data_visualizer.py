6b75c2d9f31d363ffcd05b8efbfb221abf1e4996
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2024-06-10 13:19:52 +0300

    first data visualization is tested

--- data_visualizer.py
+++ data_visualizer.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 import urllib.parse
+import matplotlib.pyplot as plt
 
 # Read the data from the CSV file
 data = pd.read_csv("data/yenimahalle/park_preprocessed.csv")
@@ -24,7 +25,46 @@ def get_lat_lon(name):
     return latitude, longitude
 
 
-# Add the latitude and longitude to the data
-data["latitude"], data["longitude"] = zip(*data["name"].map(get_lat_lon))
+# Add the latitude and longitude to the data if the .csv file is not available
+if "latitude" not in data.columns:
+    data["latitude"], data["longitude"] = zip(*data["name"].map(get_lat_lon))
 
-print(data)
+    # Save the data to a new CSV file
+    data.to_csv("data/yenimahalle/park_preprocessed.csv", index=False)
+
+# Read the latitude and longitude from the data
+latitude = data["latitude"]
+longitude = data["longitude"]
+
+# Plot the latitude and longitude on a scatter plot
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.title("Parks in Yenimahalle")
+
+# Use the area to determine the size of the points
+size = data["area"] / 10
+# plt.scatter(longitude, latitude, s=size, alpha=0.5)
+
+# Add the name of the parks to the plot as labels
+for i, name in enumerate(data["name"]):
+    plt.text(longitude[i], latitude[i], name.lower(), fontsize=8)
+
+# Remove the texts that are too close to each other
+plt.tight_layout()
+
+# Change the color of the points based on the presence of saplings, resting, sports, playground, grass
+colors = (
+    data["playground"]
+    + 2 * data["sports"]
+    + 3 * data["resting"]
+    + 4 * data["grass"]
+    + 5 * data["saplings"]
+)
+plt.scatter(longitude, latitude, s=size, c=colors, alpha=0.5)
+
+# Add a colorbar to the plot
+cbar = plt.colorbar()
+cbar.set_label("Attributes")
+
+# Save the plot to a file
+plt.savefig("data/yenimahalle/parks.png")
