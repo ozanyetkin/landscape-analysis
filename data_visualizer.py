c4e3fa718677ff15344c346ffc3f348c6a5a12e9
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2024-06-10 14:36:04 +0300

    color mix added, labels to be resolved

--- data_visualizer.py
+++ data_visualizer.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 import urllib.parse
+import matplotlib.colors
 import matplotlib.pyplot as plt
 
 # Read the data from the CSV file
@@ -53,18 +54,63 @@ for i, name in enumerate(data["name"]):
 plt.tight_layout()
 
 # Change the color of the points based on the presence of saplings, resting, sports, playground, grass
-colors = (
-    data["playground"]
-    + 2 * data["sports"]
-    + 3 * data["resting"]
-    + 4 * data["grass"]
-    + 5 * data["saplings"]
-)
-plt.scatter(longitude, latitude, s=size, c=colors, alpha=0.5)
+saplings = data["saplings"]
+resting = data["resting"]
+sports = data["sports"]
+playground = data["playground"]
+grass = data["grass"]
 
-# Add a colorbar to the plot
-cbar = plt.colorbar()
-cbar.set_label("Attributes")
+# Create a color map for the points, and store it with the labels to create a color bar legend
+labels = ["saplings", "resting", "sports", "playground", "grass"]
+colors = ["forestgreen", "blue", "red", "orange", "limegreen"]
+
+def mix_colors(color1, color2):
+    # Get the RGB values of the predefined matplotlib colors
+    r1, g1, b1 = matplotlib.colors.to_rgba(color1)[:3]
+    r2, g2, b2 = matplotlib.colors.to_rgba(color2)[:3]
+    # Mix the RGB values
+    r = (r1 + r2) / 2
+    g = (g1 + g2) / 2
+    b = (b1 + b2) / 2
+    return (r, g, b)
+
+# Create a color map for the points based on the presence of the attributes,
+# mix the colors if multiple attributes are present, store the labels and colors for the legend
+color_map = []
+for i in range(len(data)):
+    color = "white"
+    for j, attribute in enumerate([saplings, resting, sports, playground, grass]):
+        if attribute[i] == 1:
+            if color == "white":
+                color = colors[j]
+            else:
+                # Mix the colors if multiple attributes are present
+                color = mix_colors(color, colors[j])
+    color_map.append(color)
+
+# Create a color bar legend
+color_map_legend = []
+for i, label in enumerate(labels):
+    color_map_legend.append(
+        plt.Line2D(
+            [0],
+            [0],
+            marker="o",
+            color="w",
+            label=label,
+            markerfacecolor=colors[i],
+            markersize=10,
+        )
+    )
+
+# Plot the points with the color map
+plt.scatter(longitude, latitude, s=size, alpha=0.5, c=color_map)
+
+# Add the color bar legend to the plot
+plt.legend(handles=color_map_legend, loc="upper right")
+
+# Show the plot
+plt.show()
 
 # Save the plot to a file
 plt.savefig("data/yenimahalle/parks.png")
